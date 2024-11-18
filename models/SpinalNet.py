66fa749573e291dc90527c45274d67486608f04e
Stefano Bosoppi <stefano.bosoppi@outlook.com>
2024-11-18 15:18:51 +0100

    Add SpinalNet model
    Achieved 90.15 accuracy and saved the weights

---
+++ models/SpinalNet.py
@@ -0,0 +1,361 @@
+# -*- coding: utf-8 -*-
+"""
+This Script contains the default and Spinal VGG code for EMNIST(Letters).
+This code trains both NNs as two different models.
+This code randomly changes the learning rate to get a good result.
+@author: Dipu
+"""
+
+
+import math
+from os.path import isfile, join
+from pathlib import Path
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+__root_folder = Path(__file__).parents[1].resolve().as_posix()
+datasets_location = join(__root_folder, "datasets/")
+
+num_epochs = 200
+batch_size_train = 100
+batch_size_test = 1000
+learning_rate = 0.005
+momentum = 0.5
+log_interval = 500
+
+
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.EMNIST(
+        root=datasets_location,
+        split="balanced",
+        train=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.RandomPerspective(),
+                torchvision.transforms.RandomRotation(10, fill=(0,)),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    ),
+    batch_size=batch_size_train,
+    shuffle=True,
+    num_workers=8,
+)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.EMNIST(
+        datasets_location,
+        split="balanced",
+        train=False,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    ),
+    batch_size=batch_size_test,
+    shuffle=True,
+    num_workers=8,
+)
+
+# examples = enumerate(test_loader)
+# batch_idx, (example_data, example_targets) = next(examples)
+
+
+# print(example_data.shape)
+
+# import matplotlib.pyplot as plt
+
+# fig = plt.figure()
+# for i in range(6):
+#   plt.subplot(2,3,i+1)
+#   plt.tight_layout()
+#   plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+#   plt.title("Ground Truth: {}".format(example_targets[i]))
+#   plt.xticks([])
+#   plt.yticks([])
+# fig
+
+
+class VGG(nn.Module):
+    """
+    Based on - https://github.com/kkweon/mnist-competition
+    from: https://github.com/ranihorev/Kuzushiji_MNIST/blob/master/KujuMNIST.ipynb
+    """
+
+    def two_conv_pool(self, in_channels, f1, f2):
+        s = nn.Sequential(
+            nn.Conv2d(in_channels, f1, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(f1, f2, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        for m in s.children():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        return s
+
+    def three_conv_pool(self, in_channels, f1, f2, f3):
+        s = nn.Sequential(
+            nn.Conv2d(in_channels, f1, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(f1, f2, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(f2, f3, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        for m in s.children():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        return s
+
+    def __init__(self, num_classes=62):
+        super(VGG, self).__init__()
+        self.l1 = self.two_conv_pool(1, 64, 64)
+        self.l2 = self.two_conv_pool(64, 128, 128)
+        self.l3 = self.three_conv_pool(128, 256, 256, 256)
+        self.l4 = self.three_conv_pool(256, 256, 256, 256)
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(512, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.l2(x)
+        x = self.l3(x)
+        x = self.l4(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return F.log_softmax(x, dim=1)
+
+
+Half_width = 128
+layer_width = 128
+
+
+class SpinalVGG(nn.Module):
+    """
+    Based on - https://github.com/kkweon/mnist-competition
+    from: https://github.com/ranihorev/Kuzushiji_MNIST/blob/master/KujuMNIST.ipynb
+    """
+
+    def two_conv_pool(self, in_channels, f1, f2):
+        s = nn.Sequential(
+            nn.Conv2d(in_channels, f1, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(f1, f2, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        for m in s.children():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        return s
+
+    def three_conv_pool(self, in_channels, f1, f2, f3):
+        s = nn.Sequential(
+            nn.Conv2d(in_channels, f1, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(f1, f2, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(f2, f3, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(f3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        for m in s.children():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        return s
+
+    def __init__(self, num_classes=62):
+        super(SpinalVGG, self).__init__()
+        self.l1 = self.two_conv_pool(1, 64, 64)
+        self.l2 = self.two_conv_pool(64, 128, 128)
+        self.l3 = self.three_conv_pool(128, 256, 256, 256)
+        self.l4 = self.three_conv_pool(256, 256, 256, 256)
+
+        self.fc_spinal_layer1 = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(Half_width, layer_width),
+            nn.BatchNorm1d(layer_width),
+            nn.ReLU(inplace=True),
+        )
+        self.fc_spinal_layer2 = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(Half_width + layer_width, layer_width),
+            nn.BatchNorm1d(layer_width),
+            nn.ReLU(inplace=True),
+        )
+        self.fc_spinal_layer3 = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(Half_width + layer_width, layer_width),
+            nn.BatchNorm1d(layer_width),
+            nn.ReLU(inplace=True),
+        )
+        self.fc_spinal_layer4 = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(Half_width + layer_width, layer_width),
+            nn.BatchNorm1d(layer_width),
+            nn.ReLU(inplace=True),
+        )
+        self.fc_out = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(layer_width * 4, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.l2(x)
+        x = self.l3(x)
+        x = self.l4(x)
+        x = x.view(x.size(0), -1)
+
+        x1 = self.fc_spinal_layer1(x[:, 0:Half_width])
+        x2 = self.fc_spinal_layer2(
+            torch.cat([x[:, Half_width : 2 * Half_width], x1], dim=1)
+        )
+        x3 = self.fc_spinal_layer3(torch.cat([x[:, 0:Half_width], x2], dim=1))
+        x4 = self.fc_spinal_layer4(
+            torch.cat([x[:, Half_width : 2 * Half_width], x3], dim=1)
+        )
+
+        x = torch.cat([x1, x2], dim=1)
+        x = torch.cat([x, x3], dim=1)
+        x = torch.cat([x, x4], dim=1)
+
+        x = self.fc_out(x)
+
+        return F.log_softmax(x, dim=1)
+
+
+torch.serialization.add_safe_globals([SpinalVGG])
+
+
+# For updating learning rate
+def update_lr(optimizer, lr):
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+weights_file = join(__root_folder, "models/checkpoint-spinalnet.pt")
+
+
+# Train the model
+def train(
+    train_loader: torch.utils.data.DataLoader, test_loader: torch.utils.data.DataLoader
+) -> SpinalVGG:
+    device = "cuda"
+
+    model = SpinalVGG().to(device)
+
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Train the model
+    total_step = len(train_loader)
+
+    best_accuracy = 0
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if i == 499:
+                print(
+                    "Spinal Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}".format(
+                        epoch + 1, num_epochs, i + 1, total_step, loss.item()
+                    )
+                )
+
+        # Test the model
+        model.eval()
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for images, labels in test_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+            if best_accuracy >= correct / total:
+                curr_lr = learning_rate * pow(np.random.rand(1), 3).item()
+                update_lr(optimizer, curr_lr)
+                print(
+                    "Test Accuracy of SpinalNet: {} % Best: {} %".format(
+                        100 * correct / total, 100 * best_accuracy
+                    )
+                )
+            else:
+                best_accuracy = correct / total
+                best_model = model
+                torch.save(best_model, weights_file)
+                print(
+                    "Test Accuracy of SpinalNet: {} % (improvement)".format(
+                        100 * correct / total
+                    )
+                )
+
+            model.train()
+
+    return best_model.eval()
+
+
+def get_model() -> SpinalVGG:
+    if isfile(weights_file):
+        model: SpinalVGG = torch.load(weights_file, weights_only=False)
+        return model.eval()
+    else:
+        return train(train_loader, test_loader)
